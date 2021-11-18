0ad8136477f3fc0c96d7c0861128ac0c307d050f
Khaled El Hamzi <contact@elhamzi.me>
2021-11-18 01:00:13 +0100

    Fix use body-parser.

--- src/server.ts
+++ src/server.ts
@@ -4,18 +4,21 @@ const app = express();
 
 app.use(cors());
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
 interface Bug {
   id: number,
   description: string,
-  resolved?: boolean,
+  resolved: boolean,
   userId?: number,
 }
 
 const bugs: Bug[] = [
   { id: 1, description: "Bug 1", userId: 1, resolved: true },
-  { id: 2, description: "Bug 2", userId: 1 },
-  { id: 3, description: "Bug 3", userId: 2 },
-  { id: 4, description: "Bug 4" }
+  { id: 2, description: "Bug 2", userId: 1, resolved: false },
+  { id: 3, description: "Bug 3", userId: 2, resolved: false },
+  { id: 4, description: "Bug 4", resolved: false }
 ];
 
 app.get("/api/bugs", (req: Request, res: Response) => {
